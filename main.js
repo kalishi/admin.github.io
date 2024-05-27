321c720a8ee99ebabe2b04ed55cee63afbe2a3a6
Nider <44933249+kalishi@users.noreply.github.com>
2024-05-27 12:11:28 -0400

    add new data

--- main.js
+++ main.js
@@ -2,190 +2,356 @@
 var JSON_OUEST = {
   menu: [
     {
-    name: "ARRONDISSEMENT DE PORT-AU-PRINCE",
-    link: "#",
-    sub: [{
-      name: "Commune de Port-au-Prince".toUpperCase(),
+      name: "ARRONDISSEMENT DE PORT-AU-PRINCE",
       link: "#",
       sub: [{
-        name: "1re Section Turgeau",
+        name: "Commune de Port-au-Prince".toUpperCase(),
         link: "#",
-        sub: null
+        sub: [{
+          name: "1re Section Turgeau",
+          link: "#",
+          sub: null
+        }, {
+          name: "2e Section Morne l'Hôpital",
+          link: "#",
+          sub: null
+        }, {
+          name: "3e Section Martissant ",
+          link: "#",
+          sub: null
+        },
+        ]
       }, {
-        name: "2e Section Morne l'Hôpital",
+        name: "Commune de Delmas".toUpperCase(),
         link: "#",
-        sub: null
+        sub: [{
+          name: "1re Section St-Martin",
+          link: "#",
+          sub: null
+        },
+        ]
       }, {
-        name: "3e Section Martissant ",
+        name: "Commune de Carrefour".toUpperCase(),
         link: "#",
-        sub: null
+        sub: [{
+          name: "1re Section Morne Chandelle",
+          link: "#",
+          sub: null
+        }, {
+          name: "2e Section Platon Dufrene ",
+          link: "#",
+          sub: null
+        }, {
+          name: "3e Section Taifer ",
+          link: "#",
+          sub: null
+        }, {
+          name: "4e Section Procy",
+          link: "#",
+          sub: null
+        }, {
+          name: "5e Section Coupeau ",
+          link: "#",
+          sub: null
+        }, {
+          name: "6e Section Bouvier",
+          link: "#",
+          sub: null
+        }, {
+          name: "7e Section Lavalle",
+          link: "#",
+          sub: null
+        }, {
+          name: "8e Section Berly",
+          link: "#",
+          sub: null
+        }, {
+          name: "9e Section Bizoton",
+          link: "#",
+          sub: null
+        }, {
+          name: "10e Section Thor",
+          link: "#",
+          sub: null
+        }, {
+          name: "11e Section Rivière Froide   ",
+          link: "#",
+          sub: null
+        }, {
+          name: "12e Section Malanga",
+          link: "#",
+          sub: null
+        }, {
+          name: "13e Section Corail Thor   ",
+          link: "#",
+          sub: null
+        },
+        ]
       },
+      {
+        name: "Commune de Pétion-Ville".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Montagne Noire",
+          link: "#",
+          sub: null
+        }, {
+          name: "2e Section Aux Cadets ",
+          link: "#",
+          sub: null
+        }, {
+          name: "3e Section Etang du Jonc",
+          link: "#",
+          sub: null
+        }, {
+          name: "4e Section Bellevue La Montagne ",
+          link: "#",
+          sub: null
+        }, {
+          name: "5e Section Bellevue Chardonnière  ",
+          link: "#",
+          sub: null
+        },]
+      }, {
+        name: "Commune de Kenscoff ".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Nouvelle Tourraine  ",
+          link: "#",
+          sub: null
+        }, {
+          name: "2e Section Bongars",
+          link: "#",
+          sub: null
+        }, {
+          name: "3e Section Sourcailles",
+          link: "#",
+          sub: null
+        }, {
+          name: "4e Section Belle Fontaine",
+          link: "#",
+          sub: null
+        }, {
+          name: "5e Section Grand Fond ",
+          link: "#",
+          sub: null
+        },]
+      }, {
+        name: "Commune de Gressier".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Morne à Bateau",
+          link: "#",
+          sub: null
+        }, {
+          name: "2e Section Morne Chandelle",
+          link: "#",
+          sub: null
+        }, {
+          name: "3e Section Petit Boucan",
+          link: "#",
+          sub: null
+        },]
+      }, {
+        name: "Commune de Cité Soleil".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Varreux",
+          link: "#",
+          sub: null
+        }, {
+          name: "2e Section Varreux",
+          link: "#",
+          sub: null
+        },]
+      }, {
+        name: " Commune de Tabarre".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Bellevue",
+          link: "#",
+          sub: null
+        }, {
+          name: "2e Section Bellevue",
+          link: "#",
+          sub: null
+        },]
+      }
       ]
-    }, {
-      name: "Commune de Delmas".toUpperCase(),
-      link: "#",
-      sub: [{
-        name: "1re Section St-Martin",
-        link: "#",
-        sub: null
-      },
-      ]
-    }, {
-      name: "Commune de Carrefour".toUpperCase(),
-      link: "#",
-      sub: [{
-        name: "1re Section Morne Chandelle",
-        link: "#",
-        sub: null
-      }, {
-        name: "2e Section Platon Dufrene ",
-        link: "#",
-        sub: null
-      }, {
-        name: "3e Section Taifer ",
-        link: "#",
-        sub: null
-      }, {
-        name: "4e Section Procy",
-        link: "#",
-        sub: null
-      }, {
-        name: "5e Section Coupeau ",
-        link: "#",
-        sub: null
-      }, {
-        name: "6e Section Bouvier",
-        link: "#",
-        sub: null
-      },{
-        name: "7e Section Lavalle",
-        link: "#",
-        sub: null
-      }, {
-        name: "8e Section Berly",
-        link: "#",
-        sub: null
-      }, {
-        name: "9e Section Bizoton",
-        link: "#",
-        sub: null
-      }, {
-        name: "10e Section Thor",
-        link: "#",
-        sub: null
-      }, {
-        name: "11e Section Rivière Froide   ",
-        link: "#",
-        sub: null
-      }, {
-        name: "12e Section Malanga",
-        link: "#",
-        sub: null
-      },{
-        name: "13e Section Corail Thor   ",
-        link: "#",
-        sub: null
-      },
-      ]
-    },
-    {
-      name: "Commune de Pétion-Ville".toUpperCase(),
-      link: "#",
-      sub: [{
-        name: "1re Section Montagne Noire",
-        link: "#",
-        sub: null
-      }, {
-        name: "2e Section Aux Cadets ",
-        link: "#",
-        sub: null
-      }, {
-        name: "3e Section Etang du Jonc",
-        link: "#",
-        sub: null
-      }, {
-        name: "4e Section Bellevue La Montagne ",
-        link: "#",
-        sub: null
-      }, {
-        name: "5e Section Bellevue Chardonnière  ",
-        link: "#",
-        sub: null
-      }, ]
-    }, {
-      name: "Commune de Kenscoff ".toUpperCase(),
-      link: "#",
-      sub: [{
-        name: "1re Section Nouvelle Tourraine  ",
-        link: "#",
-        sub: null
-      }, {
-        name: "2e Section Bongars",
-        link: "#",
-        sub: null
-      }, {
-        name: "3e Section Sourcailles",
-        link: "#",
-        sub: null
-      }, {
-        name: "4e Section Belle Fontaine",
-        link: "#",
-        sub: null
-      }, {
-        name: "5e Section Grand Fond ",
-        link: "#",
-        sub: null
-      }, ]
-    },{
-      name: "Commune de Gressier".toUpperCase(),
-      link: "#",
-      sub: [{
-        name: "1re Section Morne à Bateau",
-        link: "#",
-        sub: null
-      }, {
-        name: "2e Section Morne Chandelle",
-        link: "#",
-        sub: null
-      }, {
-        name: "3e Section Petit Boucan",
-        link: "#",
-        sub: null
-      },]
-    },{
-      name: "Commune de Cité Soleil".toUpperCase(),
-      link: "#",
-      sub: [{
-        name: "1re Section Varreux",
-        link: "#",
-        sub: null
-      }, {
-        name: "2e Section Varreux",
-        link: "#",
-        sub: null
-      },]
-    },{
-      name: " Commune de Tabarre".toUpperCase(),
-      link: "#",
-      sub: [{
-        name: "1re Section Bellevue",
-        link: "#",
-        sub: null
-      }, {
-        name: "2e Section Bellevue",
-        link: "#",
-        sub: null
-      },,]
-    }
-    ]
-  ,
-  }]
+      ,
+    }]
 };
 
+var JSON_SUDEST = {
+  menu: [
+    {
+      name: "ARRONDISSEMENT DE JACMEL",
+      link: "#",
+      sub: [{
+        name: "Commune de JACMEL".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Bas Cap Rouge ",
+          link: "#",
+          sub: null
+        },
 
+        ]
+      },]
+    }]
+};
+
+var JSON_NORD = {
+  menu: [
+    {
+      name: "ARRONDISSEMENT DU CAP-HAITIEN",
+      link: "#",
+      sub: [{
+        name: "Commune du Cap-Haïtien".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Bande du Nord",
+          link: "#",
+          sub: null
+        },
+
+        ]
+      },]
+    }]
+};
+
+var JSON_NORDEST = {
+  menu: [
+    {
+      name: "ARRONDISSEMENT DE FORT-LIBERTE",
+      link: "#",
+      sub: [{
+        name: "Commune de Fort-Liberté".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section  Dumas",
+          link: "#",
+          sub: null
+        },
+
+        ]
+      },]
+    }]
+};
+
+var JSON_ARTIBONITE = {
+  menu: [
+    {
+      name: "ARRONDISSEMENT DES GONAIVES",
+      link: "#",
+      sub: [{
+        name: "Commune des Gonaïves".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Pont Tamarin ",
+          link: "#",
+          sub: null
+        },
+
+        ]
+      },]
+    }]
+};
+
+var JSON_CENTRE = {
+  menu: [
+    {
+      name: "ARRONDISSEMENT DE HINCHE",
+      link: "#",
+      sub: [{
+        name: "Commune de Hinche".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Juanaria",
+          link: "#",
+          sub: null
+        },
+
+        ]
+      },]
+    }]
+};
+var JSON_SUD = {
+  menu: [
+    {
+      name: "ARRONDISSEMENT DES CAYES",
+      link: "#",
+      sub: [{
+        name: "Commune DES CAYES".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Bourdet",
+          link: "#",
+          sub: null
+        },
+
+        ]
+      },]
+    }]
+};
+var JSON_GRANDEANSE = {
+  menu: [
+    {
+      name: "ARRONDISSEMENT DE JEREMIE ",
+      link: "#",
+      sub: [{
+        name: "Commune DE JEREMIE ".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Basse Voldrogue ",
+          link: "#",
+          sub: null
+        },
+
+        ]
+      },]
+    }]
+};
+var JSON_NORDOUEST = {
+  menu: [
+    {
+      name: "ARRONDISSEMENT DE PORT-DE- PAIX",
+      link: "#",
+      sub: [{
+        name: "Commune DE PORT-DE- PAIX".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Baudin",
+          link: "#",
+          sub: null
+        },
+
+        ]
+      },]
+    }]
+};
+var JSON_NIPPES = {
+  menu: [
+    {
+      name: "ARRONDISSEMENT DE MIRAGOANE ",
+      link: "#",
+      sub: [{
+        name: "Commune DE MIRAGOANE ".toUpperCase(),
+        link: "#",
+        sub: [{
+          name: "1re Section Chalon",
+          link: "#",
+          sub: null
+        },
+
+        ]
+      },]
+    }]
+};
 $(function () {
   function parseMenu(ul, menu) {
     for (var i = 0; i < menu.length; i++) {
+      console.log(menu[i]);
       var li = $(ul).append(
         '<li class="font-medium text-blue-600 dark:text-blue-500">' + menu[i].name + '</li>');
       if (menu[i].sub != null) {
@@ -197,17 +363,36 @@ $(function () {
   }
 
   var menu_ouest = $('#menu-ouest');
+  var menu_sudest = $('#menu-sudest');
+  var menu_nord = $('#menu-nord');
+  var menu_nordest = $('#menu-nordest');
+  var menu_artibonite = $('#menu-artibonite');
+  var menu_centre = $('#menu-centre');
+  var menu_sud = $('#menu-sud');
+  var menu_grandeanse = $('#menu-grandeanse');
+  var menu_nordouest = $('#menu-nordouest');
+  var menu_nippes = $('#menu-nippes');
+
   parseMenu(menu_ouest, JSON_OUEST.menu);
+  parseMenu(menu_sudest, JSON_SUDEST.menu);
+  parseMenu(menu_nord, JSON_NORD.menu);
+  parseMenu(menu_nordest, JSON_NORDEST.menu);
+  parseMenu(menu_artibonite, JSON_ARTIBONITE.menu);
+  parseMenu(menu_centre, JSON_CENTRE.menu);
+  parseMenu(menu_sud, JSON_SUD.menu);
+  parseMenu(menu_grandeanse, JSON_GRANDEANSE.menu);
+  parseMenu(menu_nordouest, JSON_NORDOUEST.menu);
+  parseMenu(menu_nippes, JSON_NIPPES.menu);
 });
 
 
-$("button").click(function (e) { 
-  e.preventDefault();
- $(this).find('div').each(function() {
-  console.log( $(this));
-  $(this).toggleClass("hidden");
-});
-});
+// $("button").click(function (e) { 
+//   e.preventDefault();
+//  $(this).find('div').each(function() {
+//   console.log( $(this));
+//   $(this).toggleClass("hidden");
+// });
+// });
 
 $(document).on('click', '.list > li ', function () {
   $(this).next('ul').toggle();
